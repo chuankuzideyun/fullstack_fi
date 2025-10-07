818e9aec025529171cf20fde56bd4ebd5bb30e34
chuankuzideyun <yn88071@163.com>
2025-10-07 13:23:52 +0200

    3.1-3.6

--- part3/index.js
+++ part3/index.js
@@ -1,92 +1,93 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+const express = require('express')
+const app = express()
+app.use(express.json())
+let notes = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
 
-morgan.token('body', (req)=>{
-    return req.method === 'POST'? JSON.stringify(req.body): '';
+app.get('/info', (request, response) => {
+    const noteL = notes.length;
+    const currentTime = new Date().toString();
+    response.send(`
+        <p>Phonebook has info for ${noteL} people</p>
+        <p>${currentTime}</p>
+    `)
 })
 
-const app = express();
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(cors());
-
-const persons = [
-    {
-        id: "1",
-        name: "Imola Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-];
-
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
-});
-
-app.get('/info', (req, res) => {
-    const personCount = persons.length;
-    const currentTime = new Date();
-
-    res.send(`
-    <p>Phonebook has info for ${personCount} people</p>
-    <p>${currentTime}</p>
-  `);
-});
-
-app.get('/api/persons/:id', (req, res)=>{
-    const id = req.params.id;
-    const person = persons.find(p=>p.id === id);
-    if(person){
-        res.json(person);
-    } else {
-        res.status(404).send({error:'Person not found'});
-    }
-});
-
-app.delete('/api/person/:id', (req, res)=>{
-    const id = req.params.id;
-    const index = persons.findIndex(p=> p.id === id);
-    if(index !== -1){
-        persons.splice(index,1);
-        res.status(204).end();
-    } else {
-        res.status(404).send({error: 'Person not found'});
-    }
-});
-
-app.post('/api/persons', (req, res)=>{
-    const body = req.body;
-    if(!body.name || !body.number){
-        return res.status(400).json({error:'Name or number is missing'});
-    }
-    if(persons.find(p=>p.name === body.name)){
-        return  res.status(400).json({error:'Name must be unique'});
-    }
-    const newPerson = {
-        id: Math.floor(Math.random() * 1000000),
-        name: body.name,
-        number: body.number
-    };
-    persons.push(newPerson);
-    res.status(201).json(newPerson);
+app.get('/api/persons', (request, response) => {
+    response.json(notes)
 })
 
-const PORT = process.env.PORT || 3001
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = Math.floor(Math.random() * 1001)
+    return maxId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Name or number missing'
+        })
+    }
+    const existingPerson = notes.find(p => p.name === body.name);
+
+    if (existingPerson) {
+        return response.status(409).json({
+            error: 'Name already exists in the phonebook'
+        });
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
