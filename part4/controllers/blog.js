3b316135c20cef108945f81495797c9c28ca8022
chuankuzideyun <yn88071@163.com>
2025-10-16 12:52:08 +0200

    4.14

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,22 +1,55 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const {Schema} = require("mongoose");
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async(request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async(request, response) => {
+    const body = request.body
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    })
+
+    try {
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    } catch(exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.get('/:id', async (request, response, next) => {
+    try {
+        const blog = await Blog.findById(request.params.id)
+        if (blog) {
+            response.json(blog)
+        } else {
+            response.status(404).end()
+        }
+    } catch(exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+    try {
+        const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+        if (deletedBlog) {
+            res.status(204).end()
+        } else {
+            res.status(404).json({ error: 'blog not found' })
+        }
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 module.exports = blogsRouter
--- part4/tests/api.test.js
+++ part4/tests/api.test.js
@@ -1,50 +1,67 @@
-const { test, after, beforeEach } = require('node:test')
-const Blog = require('../models/blog')
+const { test, beforeEach, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helpers')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+const assert = require('node:assert')
 
-const initialBlogs = [
-    {
-        title: "aaaaa",
-        author: "Anna",
-        likes: 2,
-    },
-    {
-        title: "bbbbb",
-        author: "Joy",
-        likes: 1,
-    },
-]
+beforeEach(async () => {
+    await Blog.deleteMany({})
 
-test.only('notes are returned as json', async () => {
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
 
-test.only('there are two notes', async () => {
+test('a specific blog title is within the returned blogs', async () => {
     const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, initialBlogs.length)
+    response.body.forEach(blog => {
+        assert(blog.id, 'Blog should have id property')
+    })
 })
 
-test('the first note is about HTTP methods', async () => {
-    const response = await api.get('/api/blogs')
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'Async/Await simplifies async calls',
+        author: 'John Doe',
+        url: 'http://example.com/async-await',
+        likes: 10,
+    }
 
-    const contents = response.body.map(e => e.content)
-    assert(contents.includes('HTML is easy'))
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('Async/Await simplifies async calls'))
 })
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
+//?????
+test('blog without title or url is not added', async () => {
+    const newBlog = { author: 'Jane Doe', likes: 5 }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
 })
 
 after(async () => {
