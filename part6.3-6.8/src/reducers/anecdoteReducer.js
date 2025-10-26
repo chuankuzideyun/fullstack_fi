c5c40ba6e3ab809bc7debfffd64f9b4d6bd18b8f
chuankuzideyun <yn88071@163.com>
2025-10-26 12:43:55 +0100

    part6.3-6.8

--- part6.3-6.8/src/reducers/anecdoteReducer.js
+++ part6.3-6.8/src/reducers/anecdoteReducer.js
@@ -1,15 +1,6 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = anecdote => {
+const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
@@ -17,13 +8,41 @@ const asObject = anecdote => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = [
+  asObject('If it hurts, do it more often'),
+  asObject('Adding manpower to a late software project makes it later!'),
+  asObject('Any fool can write code that a computer can understand. Good programmers write code that humans can understand.')
+]
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+const anecdoteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'VOTE':
+      { const id = action.payload
+      const anecdoteToChange = state.find(a => a.id === id)
+      const changedAnecdote = { ...anecdoteToChange, votes: anecdoteToChange.votes + 1 }
+      return state.map(a => a.id !== id ? a : changedAnecdote) }
 
-  return state
+    case 'CREATE':
+      { const newAnecdote = asObject(action.payload)
+      return [...state, newAnecdote] }
+
+    default:
+      return state
+  }
 }
 
-export default reducer
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: id
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'CREATE',
+    payload: content
+  }
+}
+
+export default anecdoteReducer
